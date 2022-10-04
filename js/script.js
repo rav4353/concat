4bd825be34a3c4cca7c1b7d27edbccb8c26fd8d8
rav4353 <ravanthsri20@gmail.com>
2022-10-04 10:32:32 +0530

    concat

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//concat
+
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+
+const children = arr1.concat(arr2); 
+document.getElementById("demo").innerHTML = children;
